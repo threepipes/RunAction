c183a78a7ba8710be04ca82857a7bd68c0456111
threepipes_s <threepipesatos@gmail.com>
2015-09-02 16:10:51 +0900

    UseFileクラスを追加

---
+++ src/com/example/runaction/UseFile.java
@@ -0,0 +1,30 @@
+package com.example.runaction;
+
+import java.io.InputStream;
+
+import android.content.Context;
+
+public class UseFile {
+	public final static int CHIP_SIZE = 32;
+    public byte[][] load(Context context) {
+        try {
+            InputStream in = context.getResources().openRawResource(R.raw.map);
+            int row = in.read();
+            int col = in.read();
+            // マップサイズを設定
+//            int width = col * CHIP_SIZE;
+//            int height = row * CHIP_SIZE;
+            // マップを作成
+            byte[][] map = new byte[row][col];
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    map[i][j] = (byte) in.read();
+                }
+            }
+            return map;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
