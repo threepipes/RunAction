cd29fdfbb6d66c8e9fb7724aacd9c9889f49067b
threepipes_s <threepipesatos@gmail.com>
2015-09-08 18:30:58 +0900

    自動アニメーションクラスを作成

---
+++ src/com/example/runaction/game/AutoAnimation.java
@@ -0,0 +1,15 @@
+package com.example.runaction.game;
+
+public class AutoAnimation {
+	// オープニングやクリア画面、ゲームオーバー画面など、自動で動く(かつ移動を伴う)アニメーションのマクロ的なもの
+	
+	private Animation animation;
+	
+	public AutoAnimation() {
+		
+	}
+	
+	private void smallJump(){
+		
+	}
+}
--- src/com/example/runaction/game/GameOverSubMode.java
+++ src/com/example/runaction/game/GameOverSubMode.java
@@ -7,30 +7,26 @@ import android.graphics.Paint;
 
 public class GameOverSubMode extends SubMode{
 	// 背景の暗さを調整するため
-//	private Paint paint;
 	private int alpha;
 	// ブラックアウトにかける時間(Frame)
 	private static final int BLACKOUT_TIME = 30;
 	private static final int ADD_ALPHA = 0xFF / BLACKOUT_TIME;
-	private int count;
-	
+	// 完全にブラックアウトした後画像表示するためのフラグ
 	private boolean lightUp;
 	
 	public GameOverSubMode(ButtonManager bm) {
 		super(bm);
-//		paint = new Paint();
 	}
 	
 	public void init(){
 		alpha = 0;
-		count = 0;
 		lightUp = false;
 	}
 	
 	@Override
 	public void update() {
-		// アニメーション値の調整(本来はデータファイルに記述すべき)
-		// アニメーションが完全にブラックアウトしたら、電気をつけるみたいに画像とボタンを表示
+		// アニメーション値の調整
+		// 完全にブラックアウトしたら、電気をつけるみたいに画像とボタンを表示
 		if(alpha < 0xFF){
 			alpha += ADD_ALPHA;
 			if(alpha > 0xFF){
